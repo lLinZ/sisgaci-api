ec93d442e0d260df44ccd6d3622340fede37e3b8
linz <linz.webdev@gmail.com>
2023-11-17 16:58:47 -0400

    degrees, sac record, sac action record

---
+++ app/Http/Controllers/SacActionRecordController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SacActionRecord;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SacActionRecordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SacActionRecord $sacActionRecord)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SacActionRecord $sacActionRecord)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SacActionRecord $sacActionRecord)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SacActionRecord $sacActionRecord)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_11_17_155300_create_sac_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sac_comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->string('origin');
+            $table->unsignedBigInteger('call_id')->nullable();
+            $table->foreign('call_id')->references('id')->on('calls')->onUpdate('cascade')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sac_comments');
+    }
+};
